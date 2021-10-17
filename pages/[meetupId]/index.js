be5406972d895a41565dc71033a3f1952e74f820
lingardium <karatascanahmet@yahoo.com>
2021-10-17 16:40:09 +0000

    ADD API Routes

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -10,4 +10,42 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticPaths(){
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: 'm1',
+        },
+     },
+     {
+       params: {
+         meetupId: 'm2',
+       },
+     },
+    ],
+  };
+}
+
+export async function getStaticProps(context){
+  //Fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/A_meeting_between_a_man_and_a_woman.jpg/1280px-A_meeting_between_a_man_and_a_woman.jpg",
+        id: meetupId,
+        title: 'First Meetup',
+        address: 'Some Street 5, Some City',
+        description: 'This is a first meetup'
+      }
+    }
+  }
+}
+
 export default MeetupDetails;
