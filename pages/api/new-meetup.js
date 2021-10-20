2729e8d57a6451f587faccc622a433e634320333
lingardium <karatascanahmet@yahoo.com>
2021-10-20 19:17:21 +0000

    ADD data

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,10 +1,25 @@
+import { MongoClient } from 'mongodb';
 // /api/new-meetup
 // POST /api/new-meetup
 
-function handler(req,res) {
+const CONNECTION_STRING = process.env.DB; 
+
+async function handler(req,res) {
     if (req.method == 'POST') {
         const data = req.body;
 
-        const {title, image, address, descripion } = data;
+        const client = await MongoClient.connect(CONNECTION_STRING)
+        const db = client.db();
+
+        const meetupsCollection = db.collection('meetups');
+        const result = await meetupsCollection.insertOne(data);
+
+        console.log(result);
+
+        client.close();
+
+        res.status(201).json({message: 'Meetup inserted!'});
     }
 }
+
+export default handler;
