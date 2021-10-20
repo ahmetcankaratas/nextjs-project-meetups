c945a18067a831997ad34b0894a2cfe0ccd54089
lingardium <karatascanahmet@yahoo.com>
2021-10-20 20:45:50 +0000

    UPDATE fixed fallback

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -38,7 +38,8 @@ export async function getStaticPaths() {
   client.close();
 
   return {
-    fallback: false,
+    //fallback: false,
+    fallback: 'blocking',
     paths: meetups.map((meetup) => ({
       params: { meetupId: meetup._id.toString() },
     }))
